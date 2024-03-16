ea1f415efc9e80d2b93e14a833353c594c445f99
covncovy <covncovy@gmail.com>
2024-03-16 18:15:54 +0800

    fix route

--- src/views/Land.vue
+++ src/views/Land.vue
@@ -17,7 +17,7 @@
   
       onMounted(async () => {
         try {
-          const response = await fetch('/public/land.json');
+          const response = await fetch('../../public/land.json');
           const data = await response.json();
           jsonData.value = data;
   
